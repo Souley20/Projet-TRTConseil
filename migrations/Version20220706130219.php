90b03ca3aed9bb59431cb6e19cf9263327e15fab
Souleymane <150073079+Souley20@users.noreply.github.com>
2024-05-08 12:26:19 +0200

    Ajout Migrations

---
+++ migrations/Version20220706130219.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220706130219 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE candidature (id INT AUTO_INCREMENT NOT NULL, candidats_id INT DEFAULT NULL, job_id INT DEFAULT NULL, INDEX IDX_E33BD3B891BD8781 (candidats_id), INDEX IDX_E33BD3B8BE04EA9 (job_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE candidature ADD CONSTRAINT FK_E33BD3B891BD8781 FOREIGN KEY (candidats_id) REFERENCES candidats (id)');
+        $this->addSql('ALTER TABLE candidature ADD CONSTRAINT FK_E33BD3B8BE04EA9 FOREIGN KEY (job_id) REFERENCES job (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE candidature');
+    }
+}
